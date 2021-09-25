96b8c9755cef73b1a1b8ea40788255b103552349
Steven Steven <steven.steven@shopify.com>
2021-09-25 00:49:39 -0400

    page structure example

---
+++ components/pageOne.jsx
@@ -0,0 +1,7 @@
+export default function PageOne() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      Page 1
+    </div>
+  )
+}
---
+++ components/pageTwo.jsx
@@ -0,0 +1,7 @@
+export default function PageTwo() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      Page 2
+    </div>
+  )
+}
