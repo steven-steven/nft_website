13938761cb0c3ed9cc79c3cf039003eeacefcd14
Steven Steven <steven.steven@shopify.com>
2021-10-10 22:36:27 -0400

    Get walletconnect working

--- components/hooks/walletConnector.jsx
+++ components/hooks/walletConnector.jsx
@@ -1,29 +1,60 @@
-import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core"
-import { injected } from "../wallet/connectors"
+import { useEffect, useState } from 'react';
+import { useWeb3React, 
+  UnsupportedChainIdError
+} from "@web3-react/core"
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected
+} from '@web3-react/injected-connector'
+import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from '@web3-react/walletconnect-connector'
+import { UserRejectedRequestError as UserRejectedRequestErrorFrame } from '@web3-react/frame-connector'
+import { injected, walletconnect } from "../wallet/connectors"
 
 export const useWalletConnector = () => {
   const { activate, deactivate, active, error } = useWeb3React()
+  const [ errorMsg, setErrorMsg ] = useState(null);
+
+  useEffect(() => {
+    if (error) {
+      setErrorMsg(getErrorMessage(error));
+    }
+  }, [error]);
+
+  function getErrorMessage(error) {
+    if (error instanceof NoEthereumProviderError) {
+      return 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.'
+    } else if (error instanceof UnsupportedChainIdError) {
+      return "You're connected to an unsupported network. Try mainnet"
+    } else if (
+      error instanceof UserRejectedRequestErrorInjected ||
+      error instanceof UserRejectedRequestErrorWalletConnect ||
+      error instanceof UserRejectedRequestErrorFrame
+    ) {
+      return 'Please authorize this website to access your Ethereum account.'
+    } else {
+      console.error(error)
+      return 'Something went wrong. Please try again / reload.'
+    }
+  }
 
   const connectToWallet = async () => {
-    try {
-      await activate(injected)
-    } catch (ex) {
-      console.log(ex)
-    }
-    if (error instanceof UnsupportedChainIdError) {
-      alert("Please make sure you are on mainnet");
-    } else if (error) {
-      alert("Something went wrong. Please try again / reload")
-    }
+    // try {
+    //   await activate(injected)
+    // } catch {
+      try {
+        await activate(walletconnect);
+      } catch (ex) {
+      }
+    // }
   }
 
   const disconnectWallet = async () => {
     try {
       await deactivate(injected)
     } catch (ex) {
-      console.log(ex)
+      console.log(ex);
     }
   }
   
-  return {connectToWallet, disconnectWallet, isConnected: active};
+  return {connectToWallet, disconnectWallet, isConnected: active, errorMsg};
 }
--- components/wallet/connectors.js
+++ components/wallet/connectors.js
@@ -1,6 +1,12 @@
 
 import { InjectedConnector } from '@web3-react/injected-connector'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 
 export const injected = new InjectedConnector({
   supportedChainIds: [4],
+})
+
+export const walletconnect = new WalletConnectConnector({
+  rpc: { 4: process.env.NEXT_PUBLIC_WALLET_CONNECT_INFURA }, // change this to prod
+  qrcode: true
 })
