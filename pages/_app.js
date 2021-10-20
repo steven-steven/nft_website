1d5007d3430db534b5f196841dbe4428bf9a364b
Steven Steven <steven.steven@shopify.com>
2021-10-19 23:27:36 -0400

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { Web3ReactProvider } from '@web3-react/core'
 import Web3 from 'web3'
+import Script from 'next/script'
 
 function getLibrary(provider) {
   return new Web3(provider)
@@ -9,6 +10,21 @@ function getLibrary(provider) {
 function MyApp({ Component, pageProps }) {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </Web3ReactProvider>
   )
